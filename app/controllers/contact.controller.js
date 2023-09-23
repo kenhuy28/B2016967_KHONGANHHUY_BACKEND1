109f36680ad46e38921b32ac36c396d985cf9187
Anh Huy <onlinetran13@gmail.com>
2023-09-23 10:16:07 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,21 +1,27 @@
-exports.create = (req, res) => {
-    res.send({ mesage: "create handler" });
+exports.create = (req, res) =>{
+	res.send({ message: "create handler" });
 };
-exports.findAll = (req, res) => {
-    res.send({ mesage: "findAll handler"});
+
+exports.findAll = (req, res) =>{
+	res.send({ message: "findAll handler" });
 };
-exports.findOne = (req, res) => {
-    res.send({ mesage: "findOne handler"});
+
+exports.findOne = (req, res) =>{
+	res.send({ message: "findOne handler" });
 };
-exports.update = (req, res) => {
-    res.send({ mesage: "update handler"});
+
+exports.update = (req, res) =>{
+	res.send({ message: "update handler" });
 };
-exports.delete = (req, res) => {
-    res.send({ mesage: "delete handler"});
+
+exports.dalete = (req, res) =>{
+	res.send({ message: "delete handler" });
 };
-exports.deleteAll = (req, res) => {
-    res.send({ mesage: "deleteAll handler"});
+
+exports.deleteAll = (req, res) =>{
+	res.send({ message: "deleteAll handler" });
 };
-exports.findAllFavorite = (req, res) => {
-    res.send({ mesage: "findAllFavorite handler" });
+
+exports.findAllFavorite = (req, res) =>{
+	res.send({ message: "findAllFavorite handler" });
 };
